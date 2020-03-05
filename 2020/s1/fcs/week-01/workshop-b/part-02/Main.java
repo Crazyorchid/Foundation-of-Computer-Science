577350eafa54c90f4bfca2e6feb4dc20399c12eb
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-03-05 00:01:04 +0000

    Firts instatiate of the data type

---
+++ 2020/s1/fcs/week-01/workshop-b/part-02/Main.java
@@ -0,0 +1,25 @@
+class Main{
+	public static void main(String arg[]){
+		//instantiate and attribute variables of type float, //
+		//double, boolean, String and char; and,//
+		float a = 33.45f;
+
+		double b = 100.2f;
+
+		boolean c = true;
+
+		String d = "Runtao";
+
+		char e = 'e';
+
+		//Print all the variables on the console;//
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+
+
+	}
+}
