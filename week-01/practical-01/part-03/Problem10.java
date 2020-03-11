a7a259ff9ed50fee1fe8a0f4c73b07b4f52c907e
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-03-11 00:58:24 +0000

    small changes

--- week-01/practical-01/part-03/Problem10.java
+++ week-01/practical-01/part-03/Problem10.java
@@ -7,7 +7,7 @@ public class Problem10{
 		String agent_message_01282 = "Who are you?";
 		String agent_message_01283 = "My name is, Bond...";
 		String agent_message_01284 = "James Bond";
-        String agent_message_01285 = agent_message_01284 + "(00" + agent_id + ")";
+		String agent_message_01285 = agent_message_01284 + "(00" + agent_id + ")";
 		
 		System.out.println(agent_message_01282);
 		System.out.println(agent_message_01283);
