1b6d2c6b9d6a9c242a2ee8f37f227ba5371e85ad
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-03-19 06:02:50 +0000

    Trial commit

---
+++ week-03/practical-exam-01/problem-01/Problem.java
@@ -0,0 +1,14 @@
+//==================================
+// Foundations of Computer Science
+// Student: Runtao Zhuge
+// id: a1778478
+// Semester:1
+// Year:2020
+// Practical Exam Number:01
+//===================================
+
+public class Problem{
+	public static void main(String[]args){
+		System.out.println("Hello World!");
+	}
+}
