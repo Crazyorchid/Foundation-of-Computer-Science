ce7a1c9e132a4f4f95c226a7de54e2c127621413
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-03-25 09:15:28 +0000

    Problem-04 added

---
+++ 2020/s1/fcs/week-03/practical-03/Problem-04/ComplexCalculator.java
@@ -0,0 +1,61 @@
+public class ComplexCalculator {
+	
+	double real; double imagine;
+	
+	ComplexCalculator(double r, double i){
+		this.real = r;
+		this.imagine = i;
+
+}
+	public static ComplexCalculator add(ComplexCalculator c1, ComplexCalculator c2) {
+		ComplexCalculator num = new ComplexCalculator(0,0);
+		
+		num.real = c1.real + c2.real;
+		num.imagine = c1.imagine + c2.imagine;
+		
+		return num;
+		
+	}
+	
+	public static ComplexCalculator multiplication(ComplexCalculator c1, ComplexCalculator c2) {
+		ComplexCalculator num = new ComplexCalculator(0,0);
+		
+		num.real = c1.real * c2.real - c1.imagine*c2.real;
+		num.imagine = c1.imagine * c2.imagine;
+		
+		return num;
+}
+	public static ComplexCalculator division(ComplexCalculator c1, ComplexCalculator c2) {
+		ComplexCalculator num = new ComplexCalculator(0,0);
+		
+		num.real = c1.real / c2.real -c1.imagine/c2.imagine;
+		num.imagine = c1.real / c2.imagine + c1.imagine/c2.real;
+		
+		return num;
+	}
+	
+	public static ComplexCalculator sub(ComplexCalculator c1, ComplexCalculator c2) {
+		ComplexCalculator num = new ComplexCalculator(0,0);
+		
+		num.real = c1.real - c2.real;
+		num.imagine = c1.imagine - c2.imagine;
+		
+		return num;
+	}
+	
+	public static void main(String[]args) {
+		ComplexCalculator c1= new ComplexCalculator(10, 9);
+		ComplexCalculator c2 = new ComplexCalculator(6, 3);
+		ComplexCalculator num = add(c1, c2);
+		ComplexCalculator num2 = multiplication(c1, c2);
+		ComplexCalculator num3 = division(c1, c2);
+		ComplexCalculator num4 = sub(c1, c2);
+		System.out.println("Sum = " + num.real + "+" + num.imagine + "i");
+		System.out.println("Multiplication = " + num2.real * num2.imagine);
+		System.out.println("Division = "+ num3.real + "/" + num3.imagine + "i");
+		System.out.println("Substraction = " + num4.real + "- (" + num4.imagine + "i)");
+		
+		
+	}
+}
+	
