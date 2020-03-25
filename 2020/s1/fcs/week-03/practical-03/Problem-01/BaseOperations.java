c0f9541178bcce83d6657fe5b1653b28c1f60067
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-03-25 02:27:08 +0000

    Problem1 changed

--- 2020/s1/fcs/week-03/practical-03/Problem-01/BaseOperations.java
+++ 2020/s1/fcs/week-03/practical-03/Problem-01/BaseOperations.java
@@ -1,38 +1,25 @@
 public class BaseOperations {
-	public static void printArray(int [] array){
-		for (int element: array) {
-			System.out.println(element);
-		}
-
-	}
-	public static void printArray2(String [] array){
-		for (String element: array) {
-			System.out.println(element);
-		}
-
-	}
-	public static void printArray3(float [] array){
-		for (float element: array) {
-			System.out.println(element);
-		}
-
-	}
-	public static void reverseArray(int[]array) {
+public static void reverseArray(int[]array) {
 		for (int i=array.length-1; i>=0; i--) {
 			System.out.println(array[i]);
 				}
-
+	}
+		
+		public static void reverseArray(float[]array) {
+			for (int i=array.length-1; i>=0; i--) {
+				System.out.println(array[i]);
+					}
+		}
+			public static void reverseArray(String[]array) {
+				for (int i=array.length-1; i>=0; i--) {
+					System.out.println(array[i]);
+						}
 
 			}
 		
 
-	public static void main(String[]args) {
-		int x [] = {4, 9, 6, 2, 3};
-		reverseArray(x);
-		printArray(x);
-
 
 
 	}
 
-}
+
