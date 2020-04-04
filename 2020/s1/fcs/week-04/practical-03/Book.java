b959c74ffac7ca991d0c442b592f45b5d2fdfc65
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-04-04 05:50:55 +0000

    problem-01

---
+++ 2020/s1/fcs/week-04/practical-03/Book.java
@@ -0,0 +1,37 @@
+public class Book {
+    String title;
+    int year;
+    String publish;
+    int page;
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public String getPublish() {
+        return publish;
+    }
+    public void setPublish(String publish) {
+        this.publish = publish;
+    }
+    public int getPage() {
+        return page;
+    }
+    public void setPage(int page) {
+        this.page = page;
+    }
+    Book(){
+        
+    }
+    Book(String title, int year, String publish, int page){
+        
+    }
+
+}
---
+++ 2020/s1/fcs/week-04/practical-03/Shape.java
@@ -0,0 +1,38 @@
+public class Shape {
+    double width;
+    double height;
+    double radius;
+    double length;
+    
+    public double getWidth() {
+        return width;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getLength() {
+        return length;
+    }
+    public void setLength(double length) {
+        this.length = length;
+    }
+    Shape(){
+        
+    }
+    Shape(double width, double height, double radius, double length){
+        
+    }
+
+}
