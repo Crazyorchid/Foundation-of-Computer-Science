20025d80af134a96540f77683dd108065090b441
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-05-10 07:19:44 +0000

    submitted

---
+++ week-07/practical-05/MySortAlg/src/InsertionSort.java
@@ -0,0 +1,19 @@
+public class InsertionSort extends MySortAlg{
+
+        //insertion sort method in descending order
+        public int[] sort(int[] array){
+            int n = array.length;
+            for(int i = 1; i<n; i++){
+                int key = array[i];
+                int j = i - 1;
+
+                while (j>=0 && array[j]<key) { //all elements bigger than key will be moved to the front
+                    array[j+1] = array[j];
+                    j = j -1;
+                }
+                array[j+1] = key;
+//this.printArray(array);
+            }
+            return array;
+        }
+    }
---
+++ week-07/practical-05/MySortAlg/src/MySortAlg.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public abstract class MySortAlg {
+    abstract int [] sort(int[] array);
+
+    public void printArray(int [] array){
+        System.out.print("[");
+        for(int i = 0; i < array.length-1; i++){
+            System.out.print(array[i]+", ");
+        }
+        System.out.print(array[array.length-1]+"]\n");
+    }
+
+}
