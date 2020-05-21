c910310d34fae543732fff882aa3949cb0bec072
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-05-21 08:22:33 +0000

    1

--- 2020/s1/fcs/week-10/practical-exam-04/project/Selection.java
+++ 2020/s1/fcs/week-10/practical-exam-04/project/Selection.java
@@ -17,7 +17,7 @@ public class Selection extends Sort {
 	}
 
 	@Override
-	public int[] sortIntByIndex(int[] arr) {
+	public int[] sortIntByIndex(int[] arr) {      //..
 		int[]index = this.getIndex(arr.length);
 
 		for (int i = 0; i < arr.length; i++) {
