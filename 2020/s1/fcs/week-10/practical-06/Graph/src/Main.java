3b5526baa271849de49570aa2741a8142b8e201b
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-05-31 07:49:32 +0000

    submitted

---
+++ 2020/s1/fcs/week-10/practical-06/Graph/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+        public static void main(String[] args)
+        {
+            System.out.println("Test 1");
+            MyGraph graph = new MyGraph();
+            int matrix[][] = {{0 , 1 , 0 , 1},
+                              {1 , 0 , 0 , 0},
+                              {0 , 0 , 0 , 1},
+                              {0 , 1 , 1 , 0}};
+
+            graph.matrixToList(matrix);
+            graph.displayAdjListArray();
+
+            System.out.println("\nTest 2");
+            int matrix2 [][] = {{0 , 1 , 0 , 1,0},
+                    {1 , 1 , 0 , 0,0},
+                    {0 , 0 , 0 , 1,1},
+                    {0 , 1 , 1 , 0,1},
+                    {0,0,2,0,0}};
+
+            MyGraph graph2 = new MyGraph();
+            graph2.matrixToList(matrix2);
+            graph2.displayAdjListArray();
+        }
+}
