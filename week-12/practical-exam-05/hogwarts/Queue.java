e1abd2a718a6b4d2e9d02dba3be0bf83b728a69a
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-06-04 08:18:04 +0000

    try5

--- week-12/practical-exam-05/hogwarts/Queue.java
+++ week-12/practical-exam-05/hogwarts/Queue.java
@@ -9,23 +9,39 @@ Year: 2020
 Practical Exam Number: 6
 ===================================*/
 public class Queue {
-    private Node back;
+    //private Node front; // points to the first element in queue
+    private Node back; // or rear, points to the last element in queue 
 
     public Queue() {
         this.back = null;
     }
+    
+    
+
+    public Node getBack() {             //getters and setters
+        return back;
+    }
 
 
+
+    public void setBack(Node back) {
+        this.back = back;
+    }
+
+
+
+    // Add element to the back
     public void enqueue(Student tmpStudent) {
         Node newNode = new Node(tmpStudent);
         newNode.setNext(this.back);
         this.back = newNode;
-
     }
 
+    // Remove element from the front
     public void dequeue() {
+        // Check if it's empty
         if (this.back == null) {
-            System.out.println(" ");
+            System.out.println("Can't remove, queue is empty");
         } else if (this.back.getNext() == null) { // if have only one element in queue
             this.back = null;
         } else {
@@ -35,21 +51,31 @@ public class Queue {
                 tmp = tmp.getNext();
             }
             // Remove first element
-            tmp.next = null;
+            tmp.setNext(null);
         }
-
     }
 
     public void displayQueue() {
+        // Check if it's empty
         int index = 0;
         if (this.back == null) {
-            System.out.println(" ");
+            System.out.println("");
         } else {
             Node tmp = this.back;
-            while (tmp != null) {
+            while(tmp != null) {
                 index++;
-                System.out.println("#" + index + " " + tmp.getInfo().getName() + " " + tmp.getInfo().getAge() + " years old" + " " + tmp.getInfo().getPeriod()+ "nd" + " " +"in Hogwarts;");
+                if(tmp.getInfo().getPeriod()==1) {
+                System.out.printf("#%d %s, %d years old, %dst year in Hogwarts;\n", index, tmp.getInfo().getName(), tmp.getInfo().getAge(), tmp.getInfo().getPeriod());
                 tmp = tmp.getNext();
+                }
+                else if(tmp.getInfo().getPeriod()==2) {
+                    System.out.printf("#%d %s, %d years old, %dnd year in Hogwarts;\n", index, tmp.getInfo().getName(), tmp.getInfo().getAge(), tmp.getInfo().getPeriod());
+                    tmp = tmp.getNext();
+                    }
+                else {
+                    System.out.printf("#%d %s, %d years old, %drd year in Hogwarts;\n", index, tmp.getInfo().getName(), tmp.getInfo().getAge(), tmp.getInfo().getPeriod());
+                    tmp = tmp.getNext();
+                    }
             }
             System.out.println();
         }
